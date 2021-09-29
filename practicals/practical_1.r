3d4fdf28109517affc3546980031dfff3018e1da
dickonfell <dickon.fell@hotmail.co.uk>
2021-09-29 13:20:19 +0100

    fleshed out split_punct, used it to seperate punctuation from words in a. Not currently functional however: loading the file works but seems to cause an infinite loop somewhere

--- practicals/practical_1.r
+++ practicals/practical_1.r
@@ -5,9 +5,36 @@ a = scan("1581-0.txt",what="character",skip=156)
 n = length(a)
 a = a[-((n-2909):n)] ## strip license
 
+# Q4
 split_punct = function(words, punctuation) {
 	## searches words (a vector) for each word containing the punctuation mark
 	## removes it from the word and adds the mark as a new entry in
 	## words after the word it came from
+	locations = grep(punctuation, words, fixed=TRUE) # indices of words containing punctuation
 
+	words = gsub(punctuation, "", words[locations], fixed=TRUE) 
+	# removes punctuation from any words
+	
+	i = 0
+	for (location in locations) {
+		# loop: append the punctuation in words after the word in which it appeared
+		# i is needed as the position of the following words will be shifted
+		# each time a new element is appended
+		words = append(words, punctuation, after=location+i)
+		i = i + 1
+	}
+	
+	return (words)
 }
+
+# Q5
+punct = c(",",".",";","!",":","?")
+for (p in punct) {
+	a = split_punct(a, p) # seperating punctuation in a
+}
+
+# Q6
+a = tolower(a)
+b = unique(a,incomparables=punct) # searches for unique words, ignores punctuation marks
+ib = match(a, b) # indices of unique words in vector of words of a
+
